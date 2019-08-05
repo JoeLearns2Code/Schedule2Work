31757de85f008e87f61e5f899f8a260e622266de
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-08-04 17:14:12 -0700

    Completed routing for Home, Manager and Profile pages.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Manager from "./pages/Manager";
+import Profile from "./pages/Profile";
 import NoMatch from "./pages/NoMatch";
 
 
@@ -11,6 +13,8 @@ function App() {
         {/* <Nav /> */}
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/manager" component={Manager} />
+          <Route exact path="/profile" component={Profile} />
           <Route component={NoMatch} />
         </Switch>
       </div>
---
+++ src/pages/Manager.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class Manager extends Component {
+
+    render() {
+        return (
+            <h1>Manager page</h1>
+
+        )
+    }
+}
+
+export default Manager;
