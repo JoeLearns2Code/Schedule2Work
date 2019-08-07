ab0a4ed49a3a8e37718f7f51ae18c05dfe4642fa
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-08-06 21:46:24 -0700

    Disabled some components to get page functional for troubleshooting.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/HomePage";
+import Home from "./pages/Home";
 import Manager from "./pages/Manager";
 import Profile from "./pages/Profile";
 import NoMatch from "./pages/NoMatch";
 
-import Login from "./pages/LoginPage/Login";
+// import Login from "./pages/LoginPage/Login";
 import './App.css';
 // import Navbar from "./components/Navbar";
 
@@ -20,7 +20,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/manager" component={Manager} />
           <Route exact path="/profile" component={Profile} />
-          <Route exact path="/login" component={Login} />
+          {/* <Route exact path="/login" component={Login} /> */}
           <Route component={NoMatch} />
         </Switch>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 // import NavBar from "../../components/Navbar/Navbar";
-import NavBar from '../../components/Navbar/index';
+// import NavBar from '../components/Navbar/index';
 // import N
 
-class HomePage extends Component {
+class Home extends Component {
     render() {
         return (
             <div className="HomePage">
-                <NavBar />
-                {/* <h2>Home Page</h2> */}
+                {/* <NavBar /> */}
+                <h2>Home Page</h2>
             </div>
         );
     }
 }
 
-export default HomePage;
+export default Home;
