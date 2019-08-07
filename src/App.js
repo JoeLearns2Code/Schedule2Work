cd623a89a261450b733b24c8b705037ab07f34db
Iman Gure <imangu99@gmail.com>
2019-08-06 20:44:18 -0700

    homenlogin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/HomePage";
 import Manager from "./pages/Manager";
 import Profile from "./pages/Profile";
 import NoMatch from "./pages/NoMatch";
+
+import Login from "./pages/LoginPage/Login";
+import './App.css';
 // import Navbar from "./components/Navbar";
 
 
@@ -12,11 +15,12 @@ function App() {
   return (
     <Router>
       <div>
-        {/* <Navbar /> */}
+        {/* {<Navbar /> } */}
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/manager" component={Manager} />
           <Route exact path="/profile" component={Profile} />
+          <Route exact path="/login" component={Login} />
           <Route component={NoMatch} />
         </Switch>
       </div>
