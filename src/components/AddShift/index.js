7adabed2b72fc6cd1b74c9727cc9ec5c6413d764
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-08-08 14:21:00 -0700

    added toggle feature to AddShifts component.

--- src/components/AddShift/index.js
+++ src/components/AddShift/index.js
@@ -1,97 +1,105 @@
 //Import React.js library
 import React from "react";
+import { UncontrolledCollapse, Button } from 'reactstrap';
 
-//JSX element used for receiving user inputs, triggering handleInputChange & handleFormSubmit functions--properties from parent page components.  "q" is the value entered by user and used for search via API GET request.
+//Add new Shift Function
 function AddShift({ scheduleID, employeeID, rolesFilled, startTime, endTime, inTime, outTime, handleInputChange, handleFormSubmit }) {
   return (
     <form>
       <div className="form-group">
-        <label htmlFor="Query">
-          <h3>Enter New Shift</h3>
+        <label htmlFor="New Shift">
+          <Button color="primary" id="toggler2" style={{ marginBottom: '1rem' }}>
+            <h3>Enter New Employee Information</h3>
+          </Button>
         </label>
-        <input
-          className="form-control"
-          id="scheduleid"
-          type="text"
-          value={scheduleID}
-          placeholder="schedule ID"
-          name="scheduleid"
-          onChange={handleInputChange}
-          required
-        />
-        <input
-          className="form-control"
-          id="employeeid"
-          type="text"
-          value={employeeID}
-          placeholder="employee ID"
-          name="employeeid"
-          onChange={handleInputChange}
-          required
-        />
-        {/* Could this be a series of checkboxes? */}
-        <input
-          className="form-control"
-          id="rolesfilled"
-          type="text"
-          value={rolesFilled}
-          placeholder="roles filled"
-          name="rolesfilled"
-          onChange={handleInputChange}
-          required
-        />
-        <input
-          className="form-control"
-          id="starttime"
-          type="text"
-          value={startTime}
-          placeholder="shift start time"
-          name="starttime"
-          onChange={handleInputChange}
-          required
-        />
-        <input
-          className="form-control"
-          id="endtime"
-          type="text"
-          value={endTime}
-          placeholder="shift end time"
-          name="endtime"
-          onChange={handleInputChange}
-          required
-        />
-        {/* Should clock-in/clockout be post-MVP?  Should this be a different page, for employees? */}
-        <input
-          className="form-control"
-          id="clockintime"
-          type="text"
-          value={inTime}
-          placeholder="clock in time"
-          name="clockin"
-          onChange={handleInputChange}
-          required
-        />
-        <input
-          className="form-control"
-          id="clockouttime"
-          type="text"
-          value={outTime}
-          placeholder="clock out time"
-          name="clockout"
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      
-      <div className="pull-right">
-        <button
-          onClick={handleFormSubmit}
-          type="submit"
-          className="btn btn-lg btn-danger float-right"
-        >
-          Submit
+        <UncontrolledCollapse toggler="#toggler2">
+          <input
+            className="form-control"
+            id="scheduleid"
+            type="text"
+            value={scheduleID}
+            placeholder="schedule ID"
+            name="scheduleid"
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            className="form-control"
+            id="employeeid"
+            type="text"
+            value={employeeID}
+            placeholder="employee ID"
+            name="employeeid"
+            onChange={handleInputChange}
+            required
+          />
+          {/* Could this be a series of checkboxes? */}
+          <input
+            className="form-control"
+            id="rolesfilled"
+            type="text"
+            value={rolesFilled}
+            placeholder="roles filled"
+            name="rolesfilled"
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            className="form-control"
+            id="starttime"
+            type="text"
+            value={startTime}
+            placeholder="shift start time"
+            name="starttime"
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            className="form-control"
+            id="endtime"
+            type="text"
+            value={endTime}
+            placeholder="shift end time"
+            name="endtime"
+            onChange={handleInputChange}
+            required
+          />
+          {/* Should clock-in/clockout be post-MVP?  Should this be a different page, for employees? */}
+          <input
+            className="form-control"
+            id="clockintime"
+            type="text"
+            value={inTime}
+            placeholder="clock in time"
+            name="clockin"
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            className="form-control"
+            id="clockouttime"
+            type="text"
+            value={outTime}
+            placeholder="clock out time"
+            name="clockout"
+            onChange={handleInputChange}
+            required
+          />
+
+          <div className="pull-right">
+            <button
+              onClick={handleFormSubmit}
+              type="submit"
+              className="btn btn-lg btn-danger float-right"
+            >
+              Submit
         </button>
+          </div>
+
+        </UncontrolledCollapse>
       </div>
+
+
     </form>
   );
 }
