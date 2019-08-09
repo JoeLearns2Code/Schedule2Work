5b21b654eb3f4b8d86b87c55669f972aecabbdad
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-08-09 11:34:39 -0700

    Updated API route for eventual POST functionality

--- src/utils/API.js
+++ src/utils/API.js
@@ -33,41 +33,7 @@ export default {
 };
 
 
-//     getEmpSchedules: function () {
-//         return axios.get('/getEmpSchedules')
-//             .then(function (response) {
-//                 return response;
-//             })
-//     },
 
-//     addEmpSchedule: function (emp_id, firstName, lastName) {
-//         return axios.post('/addEmpSchedule', {
-//             emp_id: emp_id,
-//             firstName: firstName,
-//             lastName: lastName
-//         });
-//     },
-
-//     updateEmpSchedule: function (empSchedule) {
-//         return axios.put('/updateSchedule/' + empSchedule._id, {
-//             employeeSchedule: empSchedule
-//         });
-//     },
-
-//     addEmployee: function (firstName, lastName, addressOne, addressTwo, city, state, zip, email, phone, phoneType) {
-//         return axios.post("/addEmployee", {
-//             firstName: firstName,
-//             lastName: lastName,
-//             addressOne: addressOne,
-//             addressTwo: addressTwo,
-//             city: city,
-//             state: state,
-//             zip: zip,
-//             email: email,
-//             phone: phone,
-//             phoneType: phoneType
-//         });
-//     },
 
 //     updateEmployee: function (id, firstName, lastName, addressOne, addressTwo, city, state, zip, email, phone, phoneType) {
 //         return axios.put("/updateEmployee/" + id, {
@@ -91,23 +57,5 @@ export default {
 //         });
 //     },
 
-//     removeEmployee: function (id) {
-//         return axios.put("/removeEmployee/" + id);
-//     },
 
-//     removeEmpSchedule: function (emp_id) {
-//         return axios.put("/removeEmpSchedule/" + emp_id);
-//     },
-
-//     getAnnouncements: function () {
-//         return axios.get("/getAnnouncements");
-//     },
-
-//     addAnnouncements: function (title, content) {
-//         return axios.post("/addAnnouncements", {
-//             title: title,
-//             content: content
-//         });
-//     }
-// };
 
