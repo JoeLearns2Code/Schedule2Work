b613bacfbe21149502561d7e3f0183399ffb47f6
Iman Gure <imangu99@gmail.com>
2019-08-12 12:56:29 -0700

    footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import Jumbotron from "../components/Jumbotron/index";
+import Footer from '../components/Footer';
+
 
 class Home extends Component {
     render() {
         return (
-            <Jumbotron>
-            <div className="HomePage">
-                <h2>Home Page</h2>
-
-    </div>
-            </Jumbotron>
+            <div>
+                <Jumbotron>
+                {/* <div className="HomePage">
+                    <h2>Home Page</h2>
+                </div> */}
+                    <div>
+                        
+                        {this.props.children}
+                    </div>
+                </Jumbotron>
+                <Footer />
+            </div>
+            
         );
     }
 }
+
 
 export default Home;
